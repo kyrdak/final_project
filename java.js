f4014dc65a5a01a1a793e35e7cf88e985190e354
kyrdak <kiri.krivanek@gmail.com>
2024-08-09 11:29:46 +0200

    1
    finální projekt můj první web

---
+++ java.js
@@ -0,0 +1,28 @@
+
+function greet() {
+    console.log("cus")
+    console.log("jak je?")
+}
+
+greet()
+
+
+num1 = (3)
+num2 = (5)
+
+
+function spocitej() {
+
+console.log(num1 + num2)
+
+}
+
+spocitej()
+
+
+
+
+function psani() {
+    let string = ("hello", n15);
+}
+psani()
